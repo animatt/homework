f7caf6014a774db63ae6ed1022c875b1259aed06
King Crawford <crawboy@me.com>
2018-08-11 08:19:44 -0400

    pickle rollouts generated by run_expert

--- hw1/run_expert.py
+++ hw1/run_expert.py
@@ -16,6 +16,7 @@ import tf_util
 import gym
 import load_policy
 
+
 def main():
     import argparse
     parser = argparse.ArgumentParser()
@@ -48,7 +49,7 @@ def main():
             totalr = 0.
             steps = 0
             while not done:
-                action = policy_fn(obs[None,:])
+                action = policy_fn(obs[None, :])
                 observations.append(obs)
                 actions.append(action)
                 obs, r, done, _ = env.step(action)
@@ -56,7 +57,8 @@ def main():
                 steps += 1
                 if args.render:
                     env.render()
-                if steps % 100 == 0: print("%i/%i"%(steps, max_steps))
+                if steps % 100 == 0:
+                    print("%i/%i" % (steps, max_steps))
                 if steps >= max_steps:
                     break
             returns.append(totalr)
@@ -67,6 +69,10 @@ def main():
 
         expert_data = {'observations': np.array(observations),
                        'actions': np.array(actions)}
+
+        with open(f'./rollouts/{args.envname}.pkl', 'wb') as f:
+            pickle.dump(expert_data, f)
+
 
 if __name__ == '__main__':
     main()
