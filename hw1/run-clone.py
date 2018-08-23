784263139fa80f94dfb988d973f1b67719424cad
King Crawford <crawboy@me.com>
2018-08-23 19:39:03 -0400

    run-clone.py

--- hw1/run-clone.py
+++ hw1/run-clone.py
@@ -3,6 +3,7 @@ import torch
 import gym
 import pickle
 import pandas as pd
+from utils import get_mean_var
 from behavioral_cloning import load, train
 
 from my_model import MyModel
@@ -14,31 +15,35 @@ observations, actions = load(task)
 
 N, D_in = observations.shape
 _, D_out = actions.shape
+num_eps = 4
 H = 100
 
 
 env = gym.make(task)
 num_trials = 2
 per_rollout = env.spec.timestep_limit
-
+rw_mean, rw_var = 0, 0
 
 for i_trial in range(1, num_trials + 1):
     model = MyModel(D_in, H, D_out)
 
     current_size = i_trial * per_rollout
-    train(model, observations[:current_size], actions[:current_size])
+    train(model, observations[:current_size], actions[:current_size], verbose=True)
 
-    for i_ep in range(20):
+    for i_ep in range(num_eps):
         observation = env.reset()
         for t in range(per_rollout):
 
-            env.render()
+            # env.render()
             with torch.no_grad():
                 action = model(torch.from_numpy(observation).float())
                 observation, reward, done, info = env.step(action)
 
+            rw_mean, rw_var = get_mean_var(reward, rw_mean, rw_var, t + 1)
+
             if t % 100 == 0:
                 print(f'reward = {reward}')
+                print(f'rw_mean = {rw_mean}\nrw_var = {rw_var}\n')
             if done:
                 print(f'Episode finished after {t} timesteps')
                 break
