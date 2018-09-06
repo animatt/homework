d51718a7bb5d7f4f6c198afc9aa38add64e1551c
King Crawford <crawboy@me.com>
2018-09-06 12:01:15 -0400

    script for running all code required to complete first question

---
+++ hw1/question1.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source .env/bin/activate
+
+./demo.bash
+
+environments=(Ant-v2 Walker2d-v2)
+
+set -eux
+for e in environments
+do
+    python behavioral_cloning.py $e
+    python run-clone.py $e
+done
+
+python graph-experiments.py ${environments[*]}
