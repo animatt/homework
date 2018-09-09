3e75df1fd330ab2089ce63fe2d55ad9ae79abcbe
King Crawford <crawboy@me.com>
2018-09-09 09:26:10 -0400

    implement DAgger

--- hw1/dagger.py
+++ hw1/dagger.py
@@ -2,7 +2,11 @@ import sys
 import gym
 import pickle
 import torch
+import numpy as np
+import tensorflow as tf
+import tf_util
 import load_policy
+import itertools
 from my_model import MyModel
 from behavioral_cloning import load, train
 
@@ -12,6 +16,9 @@ if len(sys.argv) < 2:
 task = sys.argv[1]
 
 observations, actions = load(task)
+
+# print('observations[:2] =', observations[:2])
+# print('actions[:2] =', actions[:2])
 
 try:
     with open(f'./clones/{task}.params.pkl', 'rb') as f:
@@ -25,31 +32,55 @@ policy_fn = load_policy.load_policy(f'./experts/{task}.pkl')
 NUM_EPS = 5
 NUM_TRIALS = 5
 rewards = []
-for i_trial in range(NUM_TRIALS):
-    model = MyModel(**params)
-    model.load_state_dict(torch.load(f'./clones/{task}.pt'))
 
-    train(model, observations, actions)
+with tf.Session():
 
-    rw_per_ep = []
-    observations = []
+    tf_util.initialize()
 
-    for ep_i in range(NUM_EPS):  # generate rollout
-        obs = env.reset()
-        done = False
-        totalr = 0
+    for i_trial in range(NUM_TRIALS):
+        model = MyModel(**params)
+    
+        train(model, observations, actions)
 
-        while not done:
-            action = model(torch.from_numpy(obs).float())
-            obs, reward, done, info = env.step(action)
-        
-            observations.append(obs)
-            totalr += reward
+        print(f'observations.shape = {observations.shape}')
+        print(f'actions.shape = {actions.shape}')
 
-        rw_per_ep.append(totalr)
-        
+        rw_per_ep = []
+        new_observations = []
+    
+        for ep_i in range(NUM_EPS):  # generate rollout
+            obs = env.reset()
+            done = False
+            totalr = 0
+    
+            while not done:
+    
+                with torch.no_grad():
+                    action = model(torch.from_numpy(obs).float())
+                    obs, reward, done, info = env.step(action)
+            
+                new_observations.append(obs)
+                totalr += reward
+    
+            rw_per_ep.append(totalr)
 
-    expert_data = [(obs, policy_fun(obs[None, :])) for obs in observations]
-    expert_data = zip(*expert_data)
+        rewards.append(np.mean(rw_per_ep))
 
-    sys.exit(expert_data)
+        expert_data = [
+            (obs, policy_fn(obs[None, :])) for obs in new_observations
+        ]
+        new_observations, new_actions = list(zip(*expert_data))
+
+        new_observations = torch.tensor(
+            [obs.tolist() for obs in new_observations]
+        )
+        new_actions = torch.tensor(list(itertools.chain.from_iterable(
+            acts.tolist() for acts in new_actions
+        )))
+
+        observations = torch.cat((observations, new_observations), 0)
+        actions = torch.cat((actions, new_actions), 0)
+
+        print(f'Completed pass #{i_trial}')
+
+print(rewards)
