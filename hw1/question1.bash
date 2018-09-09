7cb1ea09e1d8541c0cd9aad755ce3510e430c04e
King Crawford <crawboy@me.com>
2018-09-09 09:24:53 -0400

    question1.bash bug

--- hw1/question1.bash
+++ hw1/question1.bash
@@ -2,20 +2,19 @@
 
 source .env/bin/activate
 
-./demo.bash
-
 environments=(Ant-v2 Walker2d-v2)
 
 set -eux
-for e in environments
+for e in ${environments[*]}
 do
+    python run_exper.py experts/$e.pkl $e --num_rollouts=5
     python behavioral_cloning.py $e
-    python run-clone.py $e
+    python run_clone.py $e
 done
 
 python create-table.py ${environments[*]}
 
-for e in environments
+for e in ${environments[*]}
 do
     python hyper_parameter_search.py $e
 done
